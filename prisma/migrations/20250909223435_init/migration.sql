1876464aba5d2a36a193c82077dc080d25016596
Thiago Oliveira <thiagooac63@gmail.com>
2025-09-09 21:27:10 -0300

    first commit

---
+++ prisma/migrations/20250909223435_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Author" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "updateAt" DATETIME NOT NULL
+);
---
+++ src/routes/books.js
@@ -0,0 +1,37 @@
+export default async function booksRoutes(app) {
+// Schema simples de validação (JSON Schema)    
+    const bookBodySchema ={
+        type: 'object',
+        required: ['title', 'author'],
+        properties: {
+            title: { type: 'string', minLength: 1 },
+            author: { type: 'string', minLength: 1 }
+        }
+    };
+
+    const idParamSchema = {
+        type: 'object',
+        required: ['id'],
+        properties: {
+            // Mantemos como string e convertemos manualmente
+            id: { type: 'string', pattern: '^[0-9]+$'}
+        }
+    };
+
+    // Create
+    app.post('/books', { schema: { body: bookBodySchema} }, async (req, reply) => {
+        const { title, author } = req.body;
+        const book = await app.prisma.book.create({ data: { title, author} });
+        return reply.code(201).send(book);
+    });
+
+    // Read (lista)
+    app.get('/books', async (req, reply) => {       
+        const books = await app.prisma.book.findMany({
+            orderBy: { id: 'asc' }
+        });
+        return reply.send(books);
+    });
+
+    // 
+}
